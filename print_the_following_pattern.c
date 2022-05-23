5528aece450154ff7ee3c05e7e0eab97484afde5
Praveen240503 <96769580+Praveen240503@users.noreply.github.com>
2022-05-23 08:23:29 +0530

    #include<stdio.h> int main() {     int i,j,n;     scanf("%d",&n);     for(i=1;i<=n;i++)     {         for(j=n;j>=1;j--)         {             printf("%d ",j);         }         printf(" ");     } }

--- print_the_following_pattern.c
+++ print_the_following_pattern.c
@@ -1,21 +1,15 @@
 #include<stdio.h>
 int main()
 {
-    int n,i,j;
+    int i,j,n;
     scanf("%d",&n);
     for(i=1;i<=n;i++)
     {
-        for(j=1;j<=n;j++)
+        for(j=n;j>=1;j--)
         {
-            if(i==j)
-            {
-                printf("0");
-            }
-            else
-            printf("x");
+            printf("%d ",j);
         }
         printf("
 ");
-    
     }
 }
